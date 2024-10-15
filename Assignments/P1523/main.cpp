d857c6ae91a07637c99270e560bfad126ce599ea
Jered Stevens <j.stevens8185@gmail.com>
2024-10-15 13:57:55 -0500

    add leetcode 1523

---
+++ Assignments/P1523/main.cpp
@@ -0,0 +1,16 @@
+// leetcode 1523 - Count Odd Numbers in an Interval Range
+
+// Given two non-negative integers low and high. Return the 
+// count of odd numbers between low and high (inclusive).
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if((high%2 == 0) && (low%2 == 0)){
+            return (high - low) / 2;
+        }
+        else /*if (high%2 == 1) && (low%2 == 0))*/{
+            return ((high - low) / 2) + 1;
+        }
+    }
+};
